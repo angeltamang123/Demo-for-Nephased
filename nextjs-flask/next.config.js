3ce11e37d9e1d4f987f776dc41d919fc16b8b610
angel_tamang <tamangangel2057@gmail.com>
2025-02-20 10:47:57 +0545

    Prod support

--- nextjs-flask/next.config.js
+++ nextjs-flask/next.config.js
@@ -3,14 +3,11 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: '/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
-            : '/api/',
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:5328/api/:path*",
       },
-    ]
+    ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
